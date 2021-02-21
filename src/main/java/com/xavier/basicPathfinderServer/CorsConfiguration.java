ae3bce78278190e8aa2bece409a19402e722c93e
Xavier Humberg <u0893045@utah.edu>
2021-02-21 12:02:58 -0700

    disable cors

--- src/main/java/com/xavier/basicPathfinderServer/CorsConfiguration.java
+++ src/main/java/com/xavier/basicPathfinderServer/CorsConfiguration.java
@@ -1,18 +1,23 @@
 package com.xavier.basicPathfinderServer;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-@EnableWebMvc
-public class CorsConfiguration implements WebMvcConfigurer{
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/*")
-		.allowedOrigins("*")
-		.allowedMethods("*")
-		.allowedHeaders("*");
-	}
+public class CorsConfiguration {
+
+    @SuppressWarnings("deprecation")
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+            }
+        };
+    }
 }
