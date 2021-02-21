3467233a6273fea312fb53d95e4f1e417898f57c
Xavier Humberg <u0893045@utah.edu>
2021-02-21 11:47:40 -0700

    Set global cors properties

--- src/main/java/com/xavier/basicPathfinderServer/CharacterController.java
+++ src/main/java/com/xavier/basicPathfinderServer/CharacterController.java
@@ -10,7 +10,6 @@ import com.google.gson.Gson;
 public class CharacterController {
 
 	@GetMapping("/character/prosopa")
-	@CrossOrigin(origins = "https://basicpathfinderfrontend.web.app")
 	public String getProsopa() {
 		PathfinderCharacter prosopa = new PathfinderCharacter("Prosopa", "https://media.discordapp.net/attachments/526680690218106891/731649744937418792/107571564_607483240193629_5533577863028070138_n.png?width=684&height=606");
 		Gson gson = new Gson();
