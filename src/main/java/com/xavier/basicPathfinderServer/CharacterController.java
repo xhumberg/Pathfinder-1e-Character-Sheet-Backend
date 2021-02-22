6437742575265fee240eea92d7e70a837f949782
Xavier Humberg <u0893045@utah.edu>
2021-02-21 17:09:47 -0700

    Stop creating Prosopa every time we hit the endpoint

--- src/main/java/com/xavier/basicPathfinderServer/CharacterController.java
+++ src/main/java/com/xavier/basicPathfinderServer/CharacterController.java
@@ -1,5 +1,6 @@
 package com.xavier.basicPathfinderServer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -12,9 +13,13 @@ public class CharacterController {
 
 	PathfinderCharacter character;
 	
+	@Autowired
+	public CharacterController() {
+		character = Prosopa.get();
+	}
+	
 	@GetMapping("/character/prosopa")
 	public String getProsopa() {
-		character = Prosopa.get();
 		Gson gson = new Gson();
 		return gson.toJson(character.convertToJson());
 	}
